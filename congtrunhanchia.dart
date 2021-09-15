f6cfa8fa4ab159462fb193991b8d19a9bc99bc7b
dungnnwork <dungnn.work@gmail.com>
2021-09-15 15:51:40 +0700

    them file tinh toan trong dart

---
+++ congtrunhanchia.dart
@@ -0,0 +1,29 @@
+phepCong(a, b) {
+  return a + b;
+}
+
+phepTru(c, d) {
+  return c - d;
+}
+
+phepNhan(m, n) {
+  return m * n;
+}
+
+phepChiaLayNguyen(e, f) {
+  return e ~/ f;
+}
+
+phepChiaLayDu(g, h) {
+  return g % h;
+}
+
+main() {
+  var cong = phepCong(2, 3);
+  var tru = phepTru(5, 4);
+  var nhan = phepNhan(1, 3);
+  var chiaLayNguyen = phepChiaLayNguyen(9, 6);
+  var chiaLayDu = phepChiaLayDu(5, 4);
+  var lists = [cong, tru, nhan, chiaLayNguyen, chiaLayDu];
+  print('${lists}');
+}
