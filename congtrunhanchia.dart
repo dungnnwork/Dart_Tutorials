94c4aebc0c6343b1491a1d79a4b010e178bb67cb
dungnnwork <dungnn.work@gmail.com>
2021-09-27 15:46:18 +0700

    add file abstract and override

--- congtrunhanchia.dart
+++ congtrunhanchia.dart
@@ -1,1 +1,27 @@
+// abstract and override
+abstract class TechTeam {
+  double ageOfDung = 24.0;
+  double ageOfHoan = 25.0;
+  double ageOfLong = 24.0;
+  inFor();
+  totalAgeTechTeam() => ageOfDung + ageOfHoan + ageOfLong;
+}
 
+class inFormation extends TechTeam {
+  @override
+  inFor() {
+    print('age of Hoan : ${ageOfHoan} \n'
+        'age of Long : ${ageOfLong} \n'
+        'age of Dung : ${ageOfDung} \n');
+  }
+
+  @override
+  totalAgeTechTeam() => (ageOfHoan + ageOfDung + ageOfLong) * 2;
+}
+
+main() {
+  var techTeam = inFormation();
+  techTeam.inFor();
+  var totalAgex2 = techTeam.totalAgeTechTeam();
+  print(totalAgex2);
+}
--- dart_lexical/bin/dart_lexical.dart
+++ dart_lexical/bin/dart_lexical.dart
@@ -1,97 +1,27 @@
-// tạo 1 class HocSinh
-//   + có các thuộc tính :
-// - họ tên
-// - lớp
-// - ngày sinh
-// - điểm toán , điểm văn , điểm anh
-//   + có các phương thức :
-// - in thông tin học sinh
-// - tính điểm trung bình 3 môn
-// - xếp loại học lực dựa trên điểm trung bình 3 môn < 5 "yếu"; 5 <=điểm < 6.5 "trung bình"; 6.5 <=điểm < 8
-//   khá; >= 8 : giỏi
-class HocSinh {
-  // các thuộc tính để chế độ private
-  String _hoTen;
-  String _lop;
-  String _ngaySinh;
-  double _diemToan;
-  double _diemVan;
-  double _diemAnh;
+// abstract and override
+abstract class TechTeam {
+  double ageOfDung = 24.0;
+  double ageOfHoan = 25.0;
+  double ageOfLong = 24.0;
+  inFor();
+  totalAgeTechTeam() => ageOfDung + ageOfHoan + ageOfLong;
+}
 
-  // hàm khởi tạo
-  HocSinh(this._hoTen, this._lop, this._ngaySinh, this._diemToan, this._diemVan,
-      this._diemAnh);
-  // các phương thức
-  // các thông tin
-  void inThongTin() {
-    print("Họ tên : ${this._hoTen}" +
-        "\n" +
-        "Lớp : ${this._lop}" +
-        "\n" +
-        "Ngày sinh : ${this._ngaySinh}" +
-        "\n" +
-        "Điểm toán : ${this._diemToan}" +
-        "\n" +
-        "Điểm văn : ${this._diemVan}" +
-        "\n" +
-        "Điểm anh : ${this._diemAnh}" +
-        "\n");
-  } // đóng inThongTin
-
-  // tinh điểm trung bình cộng
-  double tinhDiemTrungBinh() =>
-      (this._diemToan + this._diemVan + this._diemAnh) / 3;
-  // xếp loại học sinh
-  String xepLoai() {
-    var diemTB = this.tinhDiemTrungBinh();
-    if (diemTB < 5) {
-      return "yếu";
-    } else if (diemTB < 6.5) {
-      return "trung bình";
-    } else if (diemTB < 8) {
-      return "khá";
-    } else {
-      return "giỏi";
-    }
-  } // close xepLoai
-
-  // getter
-  String get hoTen => _hoTen;
-  String get lop => _lop;
-  String get ngaySinh => _ngaySinh;
-  double get diemToan => _diemToan;
-  double get diemVan => _diemVan;
-  double get diemAnh => _diemAnh;
-  // setter
-  void set hoTen(String hoTen) {
-    this._hoTen = hoTen;
+class inFormation extends TechTeam {
+  @override
+  inFor() {
+    print('age of Hoan : ${ageOfHoan} \n'
+        'age of Long : ${ageOfLong} \n'
+        'age of Dung : ${ageOfDung} \n');
   }
 
-  void set lop(String lop) {
-    this._lop = lop;
-  }
+  @override
+  totalAgeTechTeam() => (ageOfHoan + ageOfDung + ageOfLong) * 2;
+}
 
-  void set ngaySinh(String ngaySinh) {
-    this._ngaySinh = ngaySinh;
-  }
-
-  void set diemToan(double diemToan) {
-    this._diemToan = diemToan;
-  }
-
-  void set diemVan(double diemVan) {
-    this._diemVan = diemVan;
-  }
-
-  void set diemAnh(double diemAnh) {
-    this._diemAnh = diemAnh;
-  }
-} // đóng class
-
-void main() {
-  var hocsinh = HocSinh("Nguyễn Ngọc Dũng", "12a2", "4-5-1997", 5, 5, 5);
-  hocsinh.inThongTin();
-  print("Điểm trung bình : ${hocsinh.tinhDiemTrungBinh()}" +
-      "\n" +
-      "Xếp loại : ${hocsinh.xepLoai()}");
+main() {
+  var techTeam = inFormation();
+  techTeam.inFor();
+  var totalAgex2 = techTeam.totalAgeTechTeam();
+  print(totalAgex2);
 }
